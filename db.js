d36f4a3e762db9b7788e7d93f509590bfdbcbc56
unknown <bcanikki@gmail.com>
2026-03-14 10:06:21 +0530

    work

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
-const mysql = require("mysql2");
 require("dotenv").config();
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("DB connection failed:", err);
+    console.error("DB Connection Error:", err);
   } else {
-    console.log("Connected to MySQL database");
+    console.log("MySQL Connected");
   }
 });
 

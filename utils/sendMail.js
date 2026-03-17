0ca6f7915d5e50a88b9e237ddb95d66c4d63413e
unknown <bcanikki@gmail.com>
2026-03-17 10:15:51 +0530

    work

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
+  host: process.env.NODEMAILER_HOST,
+  port: process.env.NODEMAILER_PORT,
   secure: false,
   auth: {
     user: process.env.NODEMAILER_EMAIL,
